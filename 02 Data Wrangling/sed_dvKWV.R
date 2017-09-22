01fec18aed10fde0343423390b884f06d4719f60
Kate Vaughn <kvaughn@utexas.edu>
2017-09-22 12:20:13 -0500

    This script collects suspended sediment information from daily records

---
+++ 02 Data Wrangling/sed_dvKWV.R
@@ -0,0 +1,62 @@
+# This script pulls suspended-sediment data (parameter #80154, "Suspended sediment concentration, 
+# milligrams per liter") from the NWIS water-quality database for a given set of water-quality
+# measurement locations.  Currently it collects all existing suspended-sediment data, regardless 
+# of date, but that can be narrowed down if necessary.
+#
+# Original search: 
+# https://nwis.waterdata.usgs.gov/pr/nwis/dv?cb_80154=on&format=rdb&site_no=50050900&referred_module=qw&period=&begin_date=1960-01-01&end_date=2017-09-21
+
+
+require(dplyr)
+library(data.table)
+
+setwd("~/PR/00 Docs/")
+
+# This collection lists all of the sites to collect data from.  50071000 needs to be run seaprately.
+sitelist = c(50138000, 50147800, 50136400, 50144000, 50043800, 50044810, 50053025, 50056400,
+             50058350, 50063800, 50065500, 50075000, 50110900, 50114900, 50050900)
+
+
+# This loop will collect data from each site in the collection called 'sitelist'.  
+for(i in sitelist){
+  
+  siteno <- i
+    
+    file_name = paste("seddv_", siteno, ".csv", sep = "")
+    
+    str = "~/Box Sync/PuertoRicoSedimentYields_SharedFolder/Kate's Work/Data/Originals/"
+    
+    file_path = paste(str, file_name, sep = "")
+    
+    query <- paste("https://nwis.waterdata.usgs.gov/pr/nwis/dv?cb_80154=on&format=rdb&site_no=", siteno, 
+                   "&referred_module=qw&period=&begin_date=1960-01-01&end_date=2017-09-21", sep = "")
+    
+    # Get the requested data from USGS
+    # If running by hand for 50071000, add "skip = 27L" to fread parameters
+    odf <- fread(query)
+    
+    # See what the data looks like pre-manipulation
+    str(odf)
+    
+    # Save the original file for reference
+    write.csv(odf, gsub(".csv", "_Precleaning.csv", file_path), row.names=FALSE, na = "")
+    
+    # Remove all the unnecessary columns and header information
+    df <- cbind(odf[2:nrow(odf),3], odf[2:nrow(odf),4])
+    str(df)
+    df <- data.frame(df)
+    colnames(df) <- c("date", "sedmgl")
+    
+    # Reclassify discharge column as numeric
+    df$sedmgl = as.numeric(df$sedmgl)
+    
+    # Reclassify date column as Date type
+    df$date = as.Date(as.character(df$date))
+    
+    # Write information to a new file
+    write.csv(df, gsub("Originals", siteno, file_path), row.names=FALSE, na = "")
+    
+    print(paste("Files saved for site", siteno))
+  
+}
+print("All tasks complete.")
