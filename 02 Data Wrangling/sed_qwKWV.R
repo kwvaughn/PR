5515960663513260ea2a0385528603ca8feddba4
Kate Vaughn <kvaughn@utexas.edu>
2017-09-22 12:19:34 -0500

    This script collects suspended sediment information from water quality data

--- 02 Data Wrangling/sed_qwKWV.R
+++ 02 Data Wrangling/sed_qwKWV.R
@@ -25,49 +25,49 @@ sitelist = c(50138000, 50147800, 50136400, 50144000, 50043800, 50044810, 5005302
 for(i in sitelist){
   
   siteno <- i
-    
-    file_name = paste("sed_", siteno, ".csv", sep = "")
-    
-    str = "~/Box Sync/PuertoRicoSedimentYields_SharedFolder/Kate's Work/Data/Originals/"
-    
-    file_path = paste(str, file_name, sep = "")
-    
-    query <- paste("https://nwis.waterdata.usgs.gov/nwis/qwdata?qw_count_nu=1&site_no=", siteno, "&parameter_cd=80154&inventory_output=0&rdb_inventory_output=value&begin_date=&end_date=&TZoutput=0&qw_attributes=0&format=rdb&qw_sample_wide=0&rdb_qw_attributes=0&date_format=YYYY-MM-DD&rdb_compression=value", sep = "")
-    
-    # Get the requested data from USGS
-    odf <- fread(query, skip = 72L)
-    
-    # See what the data looks like pre-manipulation
-    str(odf)
-    
-    # Save the original file for reference
-    write.csv(odf, gsub(".csv", "_Precleaning.csv", file_path), row.names=FALSE, na = "")
-    
-    # Remove all the unnecessary columns and header information
-    df <- cbind(odf[2:nrow(odf),3], odf[2:nrow(odf),4], odf[2:nrow(odf),15])
-    str(df)
-    df <- data.frame(df)
-    colnames(df) <- c("date", "time", "sedmgl")
-    
-    # Reclassify discharge column as numeric
-    df$sedmgl = as.numeric(df$sedmgl)
-    
-    # Create an hours column
-    df <- dplyr::mutate(df, hh = substr(time, 1, 2))
-    
-    # Create a minutes column
-    df <- dplyr::mutate(df, mm = substr(time, 4, 5))
-    
-    # Reclassify date column as Date type
-    df$date = as.Date(as.character(df$date))
-    
-    # Trim dataset
-    cleandf <- df %>% select(date, hh, mm, sedmgl)
-    
-    # Write information to a new file
-    write.csv(cleandf, gsub("Originals", siteno, file_path), row.names=FALSE, na = "")
-    
-    print(paste("Files saved for site", siteno))
+  
+  file_name = paste("sedqw_", siteno, ".csv", sep = "")
+  
+  str = "~/Box Sync/PuertoRicoSedimentYields_SharedFolder/Kate's Work/Data/Originals/"
+  
+  file_path = paste(str, file_name, sep = "")
+  
+  query <- paste("https://nwis.waterdata.usgs.gov/nwis/qwdata?qw_count_nu=1&site_no=", siteno, "&parameter_cd=80154&inventory_output=0&rdb_inventory_output=value&begin_date=&end_date=&TZoutput=0&qw_attributes=0&format=rdb&qw_sample_wide=0&rdb_qw_attributes=0&date_format=YYYY-MM-DD&rdb_compression=value", sep = "")
+  
+  # Get the requested data from USGS
+  odf <- fread(query, skip = 72L)
+  
+  # See what the data looks like pre-manipulation
+  str(odf)
+  
+  # Save the original file for reference
+  write.csv(odf, gsub(".csv", "_Precleaning.csv", file_path), row.names=FALSE, na = "")
+  
+  # Remove all the unnecessary columns and header information
+  df <- cbind(odf[2:nrow(odf),3], odf[2:nrow(odf),4], odf[2:nrow(odf),15])
+  str(df)
+  df <- data.frame(df)
+  colnames(df) <- c("date", "time", "sedmgl")
+  
+  # Reclassify discharge column as numeric
+  df$sedmgl = as.numeric(df$sedmgl)
+  
+  # Create an hours column
+  df <- dplyr::mutate(df, hh = substr(time, 1, 2))
+  
+  # Create a minutes column
+  df <- dplyr::mutate(df, mm = substr(time, 4, 5))
+  
+  # Reclassify date column as Date type
+  df$date = as.Date(as.character(df$date))
+  
+  # Trim dataset
+  cleandf <- df %>% select(date, hh, mm, sedmgl)
+  
+  # Write information to a new file
+  write.csv(cleandf, gsub("Originals", siteno, file_path), row.names=FALSE, na = "")
+  
+  print(paste("Files saved for site", siteno))
   
 }
 print("All tasks complete.")
